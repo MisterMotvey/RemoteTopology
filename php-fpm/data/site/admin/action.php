bbc344851a0809d2f50d27fbbbaefe56868b9010
MisterMotvey <kostarevmat@gmail.com>
2020-07-31 10:09:36 +0300

    Some updates

--- php-fpm/data/site/admin/action.php
+++ php-fpm/data/site/admin/action.php
@@ -104,7 +104,13 @@
 
                     foreach ($DEVICES_LIST as $device) {
                         $link   = $_POST[$device];
-                        $query  = $conn->query("UPDATE $table SET `$device`='$link' WHERE `Username`='$username'");
+                        try {
+                            $query  = $conn->query("UPDATE $table SET `$device`='$link' WHERE `Username`='$username'");
+                        }  catch (Exception $e) {
+                            echo 'Exception: ',  $e->getMessage(), "\n";
+                        } finally {
+                            echo "NEXT\n";
+                        }
                     }
                 }
                 else {
@@ -115,6 +121,7 @@
                     $query      = $conn->query($sql);
                     $DEVICES    = $query->fetch(PDO::FETCH_ASSOC);
                     $DEVICES_LIST = preg_split("/,/", $DEVICES[$module]);
+                    
                     // Create table name
                     $query = $conn->query("SELECT Championship FROM usersinfo.currentstate WHERE `Username` = '$username'");
                     $champ = $query->fetch(); $champ = $champ[0];
